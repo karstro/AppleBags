f02c198e5db2f3735303d162c4763ad5a03460a7
karstro <33285644+karstro@users.noreply.github.com>
2022-11-05 15:42:27 +0100

    Initial commit
    files from start.spring.io and readme

---
+++ src/main/java/Karst/Backer/AppleBags/AppleBagsApplication.java
@@ -0,0 +1,13 @@
+package Karst.Backer.AppleBags;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppleBagsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppleBagsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Karst/Backer/AppleBags/AppleBagsApplicationTests.java
@@ -0,0 +1,13 @@
+package Karst.Backer.AppleBags;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppleBagsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
